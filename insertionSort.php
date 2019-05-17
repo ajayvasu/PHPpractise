97da9b1bd0f77829242009443e6b75b99e21545e
Ajay Vasu <avasu@mtu.edu>
2019-05-17 14:06:51 -0700

    Tried booleans

---
+++ insertionSort.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Input: an R array of integers
+   Output: Sorted array R */
+
+$Rarray = array(23,62,64,3,45,21,54,99,45,9);
+
+   function insertionSort($Rarray){
+     echo "Hello World\n";
+     echo "Element 1 is {$Rarray[0]}";  
+   }
+ ?>
